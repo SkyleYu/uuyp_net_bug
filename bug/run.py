57d8b96b2133bf06efd22b0d918019498cc7fbbf
SkyleYu <skyleyu@icloud.com>
2022-04-29 15:47:02 +0800

    2022 / 4 / 29 添加了可执行的py文件

--- bug/run.py
+++ bug/run.py
@@ -22,14 +22,17 @@ sort_select.set_is_StatTrak(False)
 # 售卖价格小于799
 # sort_select.max_price = 799
 # 品质为久经沙场
-sort_select.set_abrasion_kind(3)
+# sort_select.set_abrasion_kind(3)
 # 名称中含有大马士革钢
 # sort_select.kind = "大马士革钢"
 # 期许的最小年短租收益
-sort_select.year_least = 0.4
+sort_select.year_least = 0.35
 # 期许的最小年长租收益
 # sort_select.year_long = 0.5
 
-# 输出
-for item in data_pool.sort_self(sort_select):
-    print (item)
+# 写文件
+with open("../info.txt", "w", encoding="utf-8") as file:
+    for item in data_pool.sort_self(sort_select):
+        print (item)
+        item = str(item).replace('price', "售卖价格").replace('short_unit', "短租价格").replace('long_unit', "长租价格").replace('short_get', "短租年收益").replace('long_get', "长租年收益").replace('lease_count', "出租上架数量")
+        file.write(str(item)+"\n")
